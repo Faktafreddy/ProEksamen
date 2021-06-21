52183681dbadbdde82dff46fe35a2f91e39a9584
Faktafreddy <57139233+Faktafreddy@users.noreply.github.com>
2021-06-21 13:24:01 +0200

    Add files via upload

---
+++ Eksamens spørgsmålet/script.js
@@ -0,0 +1,44 @@
+function findes(array){
+    for(let i = 0; i < array.length; i++){
+
+        for(let j = 0; j < array.length; j++){
+
+           if(i < j){
+
+                if(array[i] === array[j]){
+                    return array[i];
+                }
+
+           }
+        }
+    }
+    return false;
+}
+
+
+class undersog {
+
+    constructor(array){
+        this.array = array;
+    }
+
+    findes(){
+        for(let i = 0; i < this.array.length; i++){
+    
+            for(let j = 0; j < this.array.length; j++){
+    
+               if(i < j){
+    
+                    if(this.array[i] === this.array[j]){
+                        return this.array[i];
+                    }
+    
+               }
+            }
+        }
+        return false;
+    }
+
+}
+
+//const sog = undersog([1,2,3,4,1])
